d04965b2a0aeba076e0d6e65ec86f145ea6e7e49
Ben Matics <bmatics@viimed.com>
2015-11-30 14:05:33 -0500

    adding fix for retrieving dates from postgres with timezone attached

--- src/Database/PostgresTZConnection.php
+++ src/Database/PostgresTZConnection.php
@@ -2,6 +2,7 @@
 namespace Bmatics\PostgresTZ\Database;
 
 use Bmatics\PostgresTZ\Database\Schema\Grammars\PostgresTZGrammar as SchemaGrammar;
+use Bmatics\PostgresTZ\Database\Query\Grammars\PostgresTZGrammar as QueryGrammar;
 use Illuminate\Database\PostgresConnection;
 
 class PostgresTZConnection extends PostgresConnection
@@ -10,4 +11,9 @@ class PostgresTZConnection extends PostgresConnection
 	{
 		return $this->withTablePrefix(new SchemaGrammar);
 	}
+
+	protected function getDefaultQueryGrammar()
+    {
+        return $this->withTablePrefix(new QueryGrammar);
+    }
 }
---
+++ src/Database/Query/Grammars/PostgresTZGrammar.php
@@ -0,0 +1,17 @@
+<?php
+namespace Bmatics\PostgresTZ\Database\Query\Grammars;
+
+use Illuminate\Database\Query\Grammars\PostgresGrammar;
+
+class PostgresTZGrammar extends PostgresGrammar
+{
+    /**
+     * Get the format for database stored dates.
+     *
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:se';
+    }
+}
